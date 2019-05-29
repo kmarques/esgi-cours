ff649af03495a9a501dc83c8cc03559f7d89c7a5
Karl MARQUES <kmarques@slyvent.com>
2019-05-29 16:37:04 +0200

    lerge

--- ServerWeb/entrypoint.sh
+++ ServerWeb/entrypoint.sh
@@ -1,13 +1,25 @@
 #! /bin/bash
 
-/usr/sbin/sshd
+/usr/sbin/sshd > /dev/null 2>&1
 
-rsync --daemon
+rsync --daemon > /dev/null 2>&1
 
-nginx
+nginx > /dev/null 2>&1
 
-php-fpm7.0 -D
+php-fpm7.0 -D > /dev/null 2>&1
 
-fail2ban-client start
+fail2ban-client start > /dev/null 2>&1
 
-pg_ctlcluster 9.6 main start
+pg_ctlcluster 9.6 main start > /dev/null 2>&1
+
+postfix start > /dev/null 2>&1
+
+proftpd > /dev/null 2>&1
+
+bash /opt/.init_db.sh > /dev/null 2>&1
+
+bash /opt/.init_ufw.sh > /dev/null 2>&1
+
+echo "Initialization completed"
+
+exec "$@"
