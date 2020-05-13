642dc8e830e7e040f2fc7dae9987aa0593c1ffb9
Karl MARQUES <kmarques@vetixy.com>
2020-05-13 12:29:42 +0200

    [MOD] react-native network settings for localhost with https self-signed

--- 2019-2020/RN5IW2/App.js
+++ 2019-2020/RN5IW2/App.js
@@ -10,16 +10,35 @@ import React from 'react';
 import {SafeAreaView, StatusBar, View} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import HomeScreen from './react/HomeScreen';
+import HomeScreen2 from './react/HomeScreen2';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1, backgroundColor: 'red'}}>
-        <PaperProvider>
-          <HomeScreen />
-        </PaperProvider>
-      </SafeAreaView>
+      <PaperProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen
+              name={'Home'}
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({color}) => (
+                  <Icon name="history" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen name={'Home2'} component={HomeScreen2} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </>
   );
 };
--- 2019-2020/RN5IW2/react/HomeScreen2.js
+++ 2019-2020/RN5IW2/react/HomeScreen2.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import {Card, Title, Paragraph, Button, IconButton} from 'react-native-paper';
+import {Card, Title, Paragraph, Button} from 'react-native-paper';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const HomeScreen = ({route}) => {
+const HomeScreen = ({navigation}) => {
   const [count, setCount] = useState(null);
 
   const onSave = () => {
@@ -17,25 +17,24 @@ const HomeScreen = ({route}) => {
 
   useEffect(() => {
     AsyncStorage.getItem('count').then(data => setCount(data));
-    fetch('http://localhost:8080/posts')
-      .then(res => res.json())
-      .then(data => console.log(data));
   }, []);
 
-  console.log(route.params ? route.params.from : null);
   return (
     <Card style={styles.card}>
       <Card.Title title="Card Title" subtitle="Card Subtitle" />
       <Card.Content>
-        <Title>Card title {count} </Title>
-        <Paragraph>Card content</Paragraph>
+        <Title>Card title 2 {count} </Title>
+        <Paragraph>Card 2 content</Paragraph>
       </Card.Content>
       <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
       <Card.Actions>
         <Button onPress={() => alert('test')}>Cancel</Button>
         <Button onPress={onAdd}>+1</Button>
         <Button onPress={onSave}>Save</Button>
-        <IconButton icon="history" />
+        <Button onPress={() => navigation.goBack()}>Go Back</Button>
+        <Button onPress={() => navigation.navigate('Home', {from: 'navigate'})}>
+          Go To HomeScreen
+        </Button>
       </Card.Actions>
     </Card>
   );
